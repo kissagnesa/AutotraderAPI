3434a20f92a34405131a156b12ef6d17d0ad12df
kissagnesa <sindzse88@gmail.com>
2025-01-30 18:32:57 +0100

    cardto created

---
+++ AutotraderAPI/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using AutotraderAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutotraderAPI.Controllers
+{
+    [Route("cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult AddNewCar(CreateCarDto createcardto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ AutotraderAPI/Models/DTOs/Dto.cs
@@ -0,0 +1,4 @@
+﻿namespace AutotraderAPI.Models.DTOs
+{
+    public record CreateCarDto(string Brand, string Type, string Color, DateTime Myear);
+}
