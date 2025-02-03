0140c28eecfaf718fa39d4f0839ae2e3c83914f1
kissagnesa <sindzse88@gmail.com>
2025-02-03 15:02:56 +0100

    get, put, delete are done

--- AutotraderAPI/Controllers/CarsController.cs
+++ AutotraderAPI/Controllers/CarsController.cs
@@ -12,23 +12,84 @@ namespace AutotraderAPI.Controllers
         [HttpPost]
         public ActionResult AddNewCar(CreateCarDto createCarDto)
         {
-            var car = new Car 
-            { 
-                Id=Guid.NewGuid(),
+            var car = new Car
+            {
+                Id = Guid.NewGuid(),
                 Brand = createCarDto.Brand,
                 Type = createCarDto.Type,
                 Color = createCarDto.Color,
                 Myear = createCarDto.Myear,
             };
 
-            using (var context=new AutotraderContext())
+            using (var context = new AutotraderContext())
             {
                 context.Cars.Add(car);
                 context.SaveChanges();
 
-                return StatusCode(201, new {result=car, message="Sikeres felvétel."});
+                return StatusCode(201, new { result = car, message = "Sikeres felvétel." });
             }
-                
+
+        }
+
+        [HttpGet]
+        public ActionResult GetCars()
+        {
+            List<Car> response = new List<Car>();
+            using (var context = new AutotraderContext())
+            {
+                try
+                {
+                    response = context.Cars.ToList();
+                    return StatusCode(200, new { result = response, message = "Sikeres lekérdezés." });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+        [HttpPut]
+        public ActionResult PutCarById(Guid id, string brand, string type, string color, DateTime myear)
+        {
+            using (var context = new AutotraderContext())
+            {
+                try
+                {
+                    Car car = new Car { };
+                    car.Id = id;
+                    car.Brand = brand;
+                    car.Type = type;
+                    car.Color = color;
+                    car.Myear = myear;
+                    context.Cars.Update(car);
+                    context.SaveChanges();
+                    return StatusCode(200, new { result = car, message = "Sikeres módosítás." });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteCar(Guid id)
+        {
+            using (var context = new AutotraderContext())
+            {
+                try
+                {
+                    Car car = new Car { Id = id };
+                    context.Cars.Remove(car);
+                    context.SaveChanges();
+                    return StatusCode(200, new { result = car, message = "Sikeres törlés." });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
         }
     }
 }
